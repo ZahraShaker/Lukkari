5e2a6200fef229f14160babc9113e69160c14195
Zahra Shaker <zahrashaker@Mohamids-Air.home>
2019-02-17 14:49:20 +0200

    classes and times added

--- script/lukkari.js
+++ script/lukkari.js
@@ -1,11 +1,53 @@
 
-var maanantai = ['1. Tunti', '2. Tunti', '3. Tunti', '4. Tunti', '5. Tunti'];
-var tiistai = ['1. Tunti', '2. Tunti', '3. Tunti', '4. Tunti', '5. Tunti'];
-var keskiviikko = ['1. Tunti', '2. Tunti', '3. Tunti', '4. Tunti', '5. Tunti'];
-var torstai = ['1. Tunti', '2. Tunti', '3. Tunti', '4. Tunti', '5. Tunti'];
-var perjantai = ['1. Tunti', '2. Tunti', '3. Tunti', '4. Tunti', '5. Tunti'];
-var lauantai = ['Vapaa'];
-var sunnuntai = ['Vapaa'];
+var maanantai = [
+    {'aika':'8:15-9:15', 'tunti':'MU'},
+    {'aika':'9:30-10:30', 'tunti':'MU'},
+    {'aika':'10:50-11:50', 'tunti':'FY'},
+    {'aika':'11:50-12:15', 'tunti':'RUOKAILU'},
+    {'aika':'12:15-13:15', 'tunti':'ATK'},
+    {'aika':'13:30-14:30', 'tunti':'ATK'}
+];
+var tiistai = [
+    {'aika':'8:15-9:15', 'tunti':'TN'},
+    {'aika':'9:30-10:30', 'tunti':'TN'},
+    {'aika':'10:50-11:50', 'tunti':'FY'},
+    {'aika':'11:50-12:15', 'tunti':'RUOKAILU'},
+    {'aika':'12:15-13:15', 'tunti':'ÄI'},
+    {'aika':'13:15-14:15', 'tunti':'HYPPY'},
+    {'aika':'14:15-15:00', 'tunti':'IS'}
+];
+var keskiviikko = [
+    {'aika':'8:15-9:15', 'tunti':'HI'},
+    {'aika':'9:30-10:30', 'tunti':'ÄI'},
+    {'aika':'10:35-11:00', 'tunti':'RU'},
+    {'aika':'11:00-11:25', 'tunti':'RUOKAILU'},
+    {'aika':'11:25-12:00', 'tunti':'RU'},
+    {'aika':'12:15-13:15', 'tunti':'TE'},
+    {'aika':'13:30-14:30', 'tunti':'MA'}
+];
+var torstai = [
+    {'aika':'8:15-9:15', 'tunti':'MA'},
+    {'aika':'9:30-10:30', 'tunti':'HI'},
+    {'aika':'10:35-11:35', 'tunti':'OPO'},
+    {'aika':'11:35-12:15', 'tunti':'RUOKAILU'},
+    {'aika':'12:15-13:15', 'tunti':'LI'},
+    {'aika':'13:30-14:30', 'tunti':'LI'}
+];
+var perjantai = [
+    {'aika':'8:15-9:15', 'tunti':'MA'},
+    {'aika':'9:30-10:30', 'tunti':'HI'},
+    {'aika':'10:35-11:35', 'tunti':'FY'},
+    {'aika':'11:35-12:15', 'tunti':'RUOKAILU'},
+    {'aika':'12:15-13:15', 'tunti':'ÄI'},
+    {'aika':'13:30-14:30', 'tunti':'TE'}
+];
+var lauantai = [
+    {'aika':'-', 'tunti': 'KOODAUS'}
+];
+var sunnuntai = [
+    {'aika':'-', 'tunti': 'KOODAUS'}
+];
+
 var päivät = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai', 'Sunnuntai'];
 
 function lukkari() {
@@ -30,7 +72,7 @@ function tunnit(päivä) {
     const päiväArray = window[päivä];
     let text = "";
     for (var i = 0; i < päiväArray.length; i++) {
-        text += '<li>' + päiväArray[i] + "</li>";
+        text += '<li>' + päiväArray[i].aika +'<br/>' + päiväArray[i].tunti + "</li>";
     }
     return text;
 }
